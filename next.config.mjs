7fc5a42dfcb97231ef1b654d3c875963aabb49dc
Supertech-ITOT <itotsoftware@supertech.co.in>
2026-06-05 13:46:34 +0530

    add

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactCompiler: false,
-  devIndicators: false
+  devIndicators: false,
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:8080/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
